ae355f857073c1525f92bad41668e2962cf45f94
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-22 23:08:49 -0600

    Chore. Separate validation and implement a handle errors middleware

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { createProduct } from './handlers/product';
+import { body } from 'express-validator';
+import { handleInputErrors } from './middleware';
 
 const router = Router();
 
@@ -9,7 +11,17 @@ router.get('/', (req, res) => {
     res.json('Desde GET');
 });
 
-router.post('/', createProduct);
+router.post('/',
+    // Validación
+    body('name')
+        .notEmpty().withMessage('El nombre del producto no puede estar vacío'),
+    body('price')
+        .isNumeric().withMessage('Valor no válido')
+        .notEmpty().withMessage('El precio del producto no puede estar vacío')
+        .custom(value => value > 0).withMessage('Precio no válido'),
+    handleInputErrors, //Middleware
+    createProduct //Handler
+);
 
 router.put('/', (req, res) => {
 
