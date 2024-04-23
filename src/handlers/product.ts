6c0ee836c24d40028c0961b767a5111a5cb6602b
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-22 23:46:15 -0600

    Feat. Add put endpoint

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -30,7 +30,6 @@ export const getProductById = async (req: Request, res: Response) => {
     } catch (error) {
         console.log(error);
     }
-
 }
 
 export const createProduct = async (req: Request, res: Response) => {
@@ -41,4 +40,26 @@ export const createProduct = async (req: Request, res: Response) => {
         console.log(error);
     }
 }
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByPk(id);
+
+        if (!product) {
+            return res.status(404).json({
+                error: 'Producto no encontrado'
+            });
+        }
+
+        // Actualizar
+        await product.update(req.body); //Update puede actualizar unicamente un campo del objeto, pero lo evitamos en nuestro router con validación
+        await product.save();
+
+        res.json({ data: product });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createProduct, getProductById, getProducts } from './handlers/product';
+import { createProduct, getProductById, getProducts, updateProduct } from './handlers/product';
 import { body, param } from 'express-validator';
 import { handleInputErrors } from './middleware';
 
@@ -26,10 +26,18 @@ router.post('/',
     createProduct //Handler
 );
 
-router.put('/', (req, res) => {
-
-    res.json('Desde PUT');
-});
+router.put('/:id',
+    body('name')
+        .notEmpty().withMessage('El nombre del producto no puede estar vacío'),
+    body('price')
+        .isNumeric().withMessage('Valor no válido')
+        .notEmpty().withMessage('El precio del producto no puede estar vacío')
+        .custom(value => value > 0).withMessage('Precio no válido'),
+    body('availability')
+        .isBoolean().withMessage('Valor para disponibilidad no válido'),
+    handleInputErrors, //Middleware
+    updateProduct
+);
 
 router.patch('/', (req, res) => {
 
