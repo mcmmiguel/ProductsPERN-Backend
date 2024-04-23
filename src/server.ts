19810de7c86f0853b097930d44de55b818d1ec96
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-22 20:48:43 -0600

    Chore. Set up database connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,23 @@
 import express from 'express';
 import router from './router';
+import db from './config/db';
+
+// Conectar a base de datos
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log('Conexión exitosa');
+    } catch (error) {
+        console.log(error);
+        console.log('Error al conectar con la base de datos');
+    }
+}
+
+connectDB();
 
 const server = express();
 
-server.use('/api/products', router)
+server.use('/api/products', router);
 
 export default server;
