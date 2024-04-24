0c744e069e17925e12957c84835662bc76f4341f
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-23 19:23:53 -0600

    Test. Delete endpoint

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -132,3 +132,32 @@ describe('PUT /api/products/id', () => {
         expect(response.body).not.toHaveProperty('data');
     });
 });
+
+describe('DELETE /api/products/id', () => {
+    test('should check a valid ID', async () => {
+        const response = await request(server).delete('/api/products/not-valid');
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors[0].msg).toBe('ID no válido');
+    });
+
+    test('should return a 404 response for a non-existent product', async () => {
+        const productID = 2000;
+        const response = await request(server).delete(`/api/products/${productID}`);
+
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBe('Producto no encontrado');
+
+        expect(response.status).not.toBe(200);
+    });
+
+    test('should delete a product', async () => {
+        const response = await request(server).delete(`/api/products/1`);
+
+        expect(response.status).toBe(200);
+        expect(response.body.data).toBe('Producto eliminado');
+
+        expect(response.status).not.toBe(404);
+    })
+});
