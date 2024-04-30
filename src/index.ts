1146f047055dc7aabaccc9aa851589714b462393
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-29 22:06:55 -0600

    Fix. Port env

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import colors from 'colors'
 
 const port = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log(colors.cyan.bold(`REST API EN EL PUERTO ${port}`));
 })
